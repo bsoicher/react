caee5c52e950f6b56155df53954b1807eb06ace6
Ben Soicher <ben.soicher@protonmail.com>
2024-05-29 10:06:56 -0400

    add build for github pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import LoginGithub from 'react-login-github';
+import Example from './modal.js';
 
 const onSuccess = response => console.log(response);
 const onFailure = response => console.error(response);
 
+
+
 function App() {
   return (
     <div className="App">
+     
       <Navbar className="border-bottom">
         <Container>
           <Navbar.Brand href="https://github.com/dnd-mdn">dnd-mdn</Navbar.Brand>
@@ -20,18 +26,50 @@ function App() {
           </Nav>
           <Nav>
             <Navbar.Text>
-            <LoginGithub
+              <LoginGithub
                 className="btn btn-primary"
                 clientId="Iv1.5dceb0507b750647"
-                redirectUri="https://dnd-mdn.github.io/oauth"
+                redirectUri="https://dnd-mdn.github.io/oauth/pop.html"
                 onSuccess={onSuccess}
                 onFailure={onFailure}
               />
-              </Navbar.Text>
+            </Navbar.Text>
           </Nav>
         </Container>
       </Navbar>
+      <Example />
+        <DropdownButton
+          id="dropdown-button-dark-example2"
+          variant="secondary"
+          title="Light dropdown"
+          className="mt-2"
+          data-bs-theme="light"
+        >
+          <Dropdown.Item href="#/action-1" active>
+            Action
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+        </DropdownButton>
 
+        <DropdownButton
+          id="dropdown-button-dark-example2"
+          variant="secondary"
+          title="Dark dropdown"
+          className="mt-2"
+          data-bs-theme="dark"
+        >
+          <Dropdown.Item href="#/action-1" active>
+            Action
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+        </DropdownButton>
+      
     </div>
 
   );
